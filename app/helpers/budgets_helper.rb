0443ef8f9f384e0e3059c09e6b21b415ca75bd9b
roberto rueda <rueda.roberto89@gmail.com>
2020-02-10 22:17:18 -0500

    Create expenses per budget module

--- app/helpers/budgets_helper.rb
+++ app/helpers/budgets_helper.rb
@@ -1,2 +1,9 @@
 module BudgetsHelper
+  def expenses_per_budget(budget)
+    if budget.expenses_per_budget
+      budget.expenses_per_budget.to_s
+    else
+      0
+    end
+  end
 end
--- app/models/budget.rb
+++ app/models/budget.rb
@@ -6,4 +6,12 @@ class Budget < ApplicationRecord
   def self.total_budget
     pluck(:amount).reduce(:+)
   end
+
+  def expenses_per_budget
+    expenses_per_budget = []
+    expenses.each do |e|
+      expenses_per_budget << e.amount
+    end
+    expenses_per_budget.reduce(:+)
+  end
 end
