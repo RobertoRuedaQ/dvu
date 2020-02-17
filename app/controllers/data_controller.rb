b40938c7d129b144fc6d783de12461bf015c5056
roberto rueda <rueda.roberto89@gmail.com>
2020-02-17 12:04:21 -0500

    Update calendar and navbar

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,6 +1,6 @@
 class DataController < ApplicationController
   def index
-    @activities = Activity.activities_of_the_month.to_a
+    @activities = Activity.activities_of_the_month.to_a.sort!
     @all_participants = Activity.all_participants.to_s
     @uniq_participants_count = Activity.uniq_participants.count
   end
