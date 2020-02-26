c13ec40f2ed671d381ad55182fb9023196f744d5
roberto rueda <rueda.roberto89@gmail.com>
2020-02-26 16:02:47 -0500

    Refactor mailers

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -1,9 +1,5 @@
 class NewsletterMailer < ApplicationMailer
-  def notify
-    @users = User.all
-    @activities = Activity.activities_of_the_week.order(:start_date)
-    @users.each do |user|
-      @user = user
+  def notify(user)
       mail to: user.email, subject: "Nuestras actividades de esta semana"
     end
   end
