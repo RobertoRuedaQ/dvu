661cb483a0b604938e3ded7d15263038664014e4
roberto rueda <rueda.roberto89@gmail.com>
2020-02-26 16:04:24 -0500

    Call activities per week in calendar

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,6 +1,6 @@
 class DataController < ApplicationController
   def index
-    @activities = Activity.activities_of_the_month.order(:start_date).to_a
+    @activities = Activity.activities_of_the_week.order(:start_date).to_a
     @all_participants = Activity.all_participants.to_s
     @uniq_participants_count = Activity.uniq_participants.count
   end
