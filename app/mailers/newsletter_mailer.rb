47d6db25f347ae7d80a7f4fc48d2ca0711be59c3
roberto rueda <rueda.roberto89@gmail.com>
2020-02-26 17:24:51 -0500

    Fix error in newsletter_mailer

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -1,6 +1,5 @@
 class NewsletterMailer < ApplicationMailer
   def notify(user)
-      mail to: user.email, subject: "Nuestras actividades de esta semana"
-    end
+    mail to: user.email, subject: "Nuestras actividades de esta semana"
   end
 end
