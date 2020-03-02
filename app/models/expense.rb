4d700512c19565dd30b0865478c393acff99e711
roberto rueda <rueda.roberto89@gmail.com>
2020-03-02 11:52:34 -0500

    Fix typo in expenses model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,7 +2,7 @@ class Expense < ApplicationRecord
 EXPENSES_TYPES = ["Compra", "Pago", "Gasto"]
 CLIENTS_TYPES = ["Externo", "Interno"]
 
-validates_precense_of :expense_type, :client_type, :name, :bill_number, :amount, :budget_id, :description, :date, :activity_id, presence: true
+validates_presence_of :expense_type, :client_type, :name, :bill_number, :amount, :budget_id, :description, :date, :activity_id, presence: true
   belongs_to :activity
   belongs_to :budget
 
